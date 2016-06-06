2b44487dd35ac07cb074183647c974c45a0346c0
tschulz <timo.schulz@1und1.de>
2016-06-06 10:02:22 +0200

    add testing directorys and first test (not yet written)

---
+++ src/test/java/org/telegram/telegrambots/TelegramBotsApiTest.java
@@ -0,0 +1,17 @@
+package org.telegram.telegrambots;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by tschulz on 06.06.16.
+ */
+public class TelegramBotsApiTest extends TestCase {
+
+    public void testRegisterBot() throws Exception {
+
+    }
+
+    public void testRegisterBot1() throws Exception {
+
+    }
+}
